cc4a3dce356b80f29f63b30cdb66d56bcfc02d69
Seyma Bardakci <bostanciseyma@gmail.com>
2023-03-06 15:59:59 +0000

    xray is ok

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -33,7 +33,6 @@ provider.add_span_processor(processor)
 # x-ray
 xray_url = os.getenv("AWS_XRAY_URL")
 xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
-XRayMiddleware(app, xray_recorder)
 
 simple_processor = SimpleSpanProcessor(ConsoleSpanExporter())
 provider.add_span_processor(simple_processor)
@@ -42,6 +41,9 @@ trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
 app = Flask(__name__)
+
+# x-ray
+XRayMiddleware(app, xray_recorder)
 
 # Initialize automatic instrumentation with Flask
 FlaskInstrumentor().instrument_app(app)
